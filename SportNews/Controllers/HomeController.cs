2bce311077ec3c13f567496f41ec86d37aec8f6e
SUTHAY <lebanhatminhk14@siu.edu.vn>
2024-04-06 11:25:59 +0700

    them giao dien trang detail bao

--- SportNews/Controllers/HomeController.cs
+++ SportNews/Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace SportNews.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult DetailsBaiBao()
+        {
+            return View();
+        }
     }
 }
